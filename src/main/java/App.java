2f546c4f18cea2ac04e27e0e8daf70007ca1d156
Blake Scala <blakescala@gmail.com>
2016-09-23 13:59:26 -0700

    add functionality for adding stylists

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -21,5 +21,19 @@ public class App {
       model.put("template", "templates/add-stylist.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    post("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      String name = request.queryParams("name");
+      String email = request.queryParams("email");
+      String phone = request.queryParams("phone");
+      String availability = request.queryParams("availability");
+      String speciality = request.queryParams("speciality");
+      Stylist stylist = new Stylist(name, email, phone, availability, speciality);
+      stylist.save();
+      model.put("stylists", Stylist.all());
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
