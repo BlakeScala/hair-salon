47ac728512320e2a43401bbc3c3d98a28ff8b680
Blake Scala <blakescala@gmail.com>
2016-09-23 07:42:38 -0700

    write constructor for stylist

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -1,7 +1,17 @@
 import org.sql2o.*;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Stylist {
-  
+  private int id;
+  private String name;
+  private String email;
+  private String phone;
+  private String availability;
+  private String speciality;
+
+  public Stylist(String name, String email, String phone, String availability, String speciality) {
+    
+  }
+
 }
