a1134d364a8597ae6b0c01704ae041b8154e2bbf
Blake Scala <blakescala@gmail.com>
2016-09-23 09:27:27 -0700

    write instantiation test for client

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,7 +1,19 @@
 import org.sql2o.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Client {
-  
+  private int id;
+  private String name;
+  private String phone;
+  private String email;
+  private int stylistId;
+
+  public Client(String name, String phone, String email, int stylistId) {
+    this.name = name;
+    this.phone = phone;
+    this.email = email;
+    this.stylistId = stylistId;
+  }
 }
---
+++ src/test/java/ClientTest.java
@@ -0,0 +1,17 @@
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class ClientTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void Client_instantiatesCorrectly() {
+    Client client = new Client("Cindy", "878-484-2947", "cindyt@gmail.com", 1);
+    assertTrue(client instanceof Client);
+  }
+}
